0ff1d637e622040bca7b8a6556246af6473f44a6
Xavier-Julian-Rodriguez <xavierjulianrod@gmail.com>
2023-08-15 14:00:33 -0400

    added coverage, specifications, and threashold

---
+++ src/__tests__/greet-test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react"
+import { Greet } from "../components/greet/greet"
+
+
+describe('Greet', () => {
+    test('renders correctly', () => {
+        render(<Greet />)
+        const textElement = screen.getByText('Hello')
+        expect(textElement).toBeInTheDocument()
+    })
+    
+    test('renders with a name', () => {
+        render(<Greet name="Xavier"/>)
+        const textElement = screen.getByText('Hello Xavier')
+        expect(textElement).toBeInTheDocument()
+    })
+})
---
+++ src/components/greet/greet.spec.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react"
+import { Greet } from "./greet"
+
+
+describe('Greet', () => {
+    it('renders correctly', () => {
+        render(<Greet />)
+        const textElement = screen.getByText('Hello')
+        expect(textElement).toBeInTheDocument()
+    })
+    
+    it('renders with a name', () => {
+        render(<Greet name="Xavier"/>)
+        const textElement = screen.getByText('Hello Xavier')
+        expect(textElement).toBeInTheDocument()
+    })
+})
