4aa13685abfd77e52fc48be8486c03c946df39f7
Xavier-Julian-Rodriguez <xavierjulianrod@gmail.com>
2023-08-15 12:06:08 -0400

    added the describe method to group 2 tests.

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,14 +1,17 @@
 import { render, screen } from "@testing-library/react"
 import { Greet } from "./greet"
 
-test('Greet renders correctly', () => {
-    render(<Greet />)
-    const textElement = screen.getByText('Hello')
-    expect(textElement).toBeInTheDocument()
-})
 
-test('Greet renders with a name', () => {
-    render(<Greet name="Xavier"/>)
-    const textElement = screen.getByText('Hello Xavier')
-    expect(textElement).toBeInTheDocument()
+describe('Greet', () => {
+    test('renders correctly', () => {
+        render(<Greet />)
+        const textElement = screen.getByText('Hello')
+        expect(textElement).toBeInTheDocument()
+    })
+    
+    test('renders with a name', () => {
+        render(<Greet name="Xavier"/>)
+        const textElement = screen.getByText('Hello Xavier')
+        expect(textElement).toBeInTheDocument()
+    })
 })
